d635e97f2ab000ac1e15352187f0b9989d2c08e0
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2020-03-29 22:43:53 -0300

    initial commit

--- criptografia.js
+++ criptografia.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const sha1 = require('sha1');
 
 const getDataUrl = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=';
 const token = '03fa9e9517555f15a0d6349b252a9024be4c517f';
@@ -25,9 +26,19 @@ const decrypt = (crypted, number) => {
 
 axios.get(getDataUrl+token)
     .then((result) => {
-        fs.appendFile('answer.json', JSON.stringify(result.data), (error) => {
-            console.log(decrypt(result.data.cifrado, result.data.numero_casas));
-        });
+        const exist = fs.existsSync('answer.json');
+
+        if(!exist){
+            fs.appendFileSync('answer.json', JSON.stringify(result.data));
+        }
+
+        const decryptedPhrase = decrypt(result.data.cifrado, result.data.numero_casas);
+
+        result.data.decifrado = decryptedPhrase;
+
+        result.data.resumo_criptografico = sha1(decryptedPhrase);
+
+        fs.writeFileSync('answer.json', JSON.stringify(result.data));
     });
 
 
